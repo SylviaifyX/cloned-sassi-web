a0db87b7f6215c1a67c7a8e411fe3dc20fecb49e
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-05-19 18:03:48 +0100

    work in progress on the product page

--- public/main.js
+++ public/main.js
@@ -23,19 +23,6 @@ function close(){
     dropMenu.style.top = "-100%"
 }
 
-//first grid-card-section//
-// let cardImage = document.querySelector(".cardA-img");
-// let overlay = document.querySelector(".top")
-// cardImage.addEventListener('mouseenter', () =>{
-//     overlay.style.display ="block";
-//     cardImage.style.display ="none"
-// })
-// cardImage.addEventListener("mouseleave", () =>{
-//     overlay.style.display ="none";
-//     cardImage.style.display ="block";
-// })
-
-
 
 // handling cart container/
 let cartIcon = document.getElementById("cartbtn")
@@ -59,3 +46,39 @@ closeBtn.addEventListener("click", () =>{
     addCartCon.style.transition = '.9s ease-out'
     addCartCon.style.inset = '0 -500px 0 auto'
 })
+
+//handling the product page to be able to add to cart//
+let productList = document.querySelector(".fashion-item-container")
+let listProduct = []
+
+
+const showData = () =>{
+    productList.innerHTML = "";
+
+    if( listProduct.length > 0){
+        listProduct.forEach(product =>{
+            let newProductList = document.createElement('div')
+            newProductList.classList.add('fashion-item')
+            newProductList.innerHTML =`
+            <div class="fashion-item p-4 flex flex-col md:grid-cols-3 sm:grid-cols-2 gap-2">
+            <img src="${product.image}" class="image" alt=""></img>
+            <h2 class="font-bold">${product.tittle}</h2>
+            <div class="price text-[18px] text-slate-300 font-bold">${product.price}</div>
+            <button class="addcart-button">Add To Cart</button>
+            </div>
+            `
+            productList.appendChild(newProductList)
+
+            // console.log(productList)
+        })
+    }
+}
+const getData = () =>{
+    fetch('dummyData.json').then(response => response.json())
+    .then(data =>{
+        listProduct = data
+        showData()
+        // console.log(data)
+    })
+}
+getData()
